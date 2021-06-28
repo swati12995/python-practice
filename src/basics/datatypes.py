e9936bd1cb33bf669b20cbac646f5f2a25f0f394
s_sinha <swatisinhaanurag.kv@gmail.com>
2021-06-28 18:06:09 +0100

    datatypes.py strings.py

---
+++ src/basics/datatypes.py
@@ -0,0 +1,16 @@
+# Single lime comment starts with # (hash)
+''' This is multi line comment:
+lin1
+line2
+line3...
+'''
+""" This is also a multi line comment
+line1 
+line2
+line3...
+"""
+# Types of Data Types in Python
+# numeric
+print("Helo world")
+a = 0.1 + 0.2 - 0.3
+print(a)
---
+++ src/basics/strings.py
@@ -0,0 +1,28 @@
+''' 
+ordered sequence of characters, using either single quotes or double quotes:
+'hello'
+"hello"
+we can use indexing and slicing to grab subsection
+indexing notation: []
+'''
+print('hello')
+print("hello")
+print('This is also \n a string')
+# print('' i'm going to run')
+print("String \t Practice")
+print(len("hello world"))
+mystring = "Python Programming"
+print(mystring[0])
+print(mystring[8])
+print(len(mystring))
+print(mystring[-1])
+print(mystring[-2])
+print(mystring[-18])
+print(mystring[1:])
+print(mystring[-18:])
+print(mystring[:6])
+print(mystring[1:9])
+print(mystring[::2])
+print(mystring[::3])
+print(mystring[2:18:2])
+print(mystring[::-1])
