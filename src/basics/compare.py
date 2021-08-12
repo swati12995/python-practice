892fd0079a284fed8d9d140bbdb1ae5e46aa3647
s_sinha <swatisinhaanurag.kv@gmail.com>
2021-08-12 15:36:57 +0100

    Python Statements 	new file:   src/basics/compare.py 	new file:   src/basics/forloops.py 	new file:   src/basics/statements.py

---
+++ src/basics/compare.py
@@ -0,0 +1,27 @@
+print(2 == 2)
+print('hello' == 'hello')
+print('hello' == 'bye')
+print('hello' == 'Hello')
+print('2' == 2)
+print(2.0 == 2)
+print(3 != 3)
+print(4 != 9)
+print(1 < 2)
+print(7 <= 9)
+'''logical operator
+we can use logical operators to combine comparisons:
+* and
+* or
+* not
+'''
+print(1 < 2 < 3)
+print(1 < 2 > 3)
+print(1 < 2 and 2 < 3)
+print(('h' == 'h') and (2 == 2))
+print(4 == 1 or 2 == 2)
+print("output ", not 1 == 1)
+a = 12
+b = a-10
+print("a=", a, "\nb=", b, "\n", a > b)
+print(2 < 3 > 10)
+print(2 <= 3 >= 1)
---
+++ src/basics/forloops.py
@@ -0,0 +1,15 @@
+'''
+for loop is used to execute a block of code for every iteration
+syntax of a for loop
+my_iterable =[1,2,3]
+for item_name in my_iterable:
+    print(item_name)
+'''
+myli = [3, 4, 5, 6, 7, 8, 10]
+for itn in myli:
+    print(itn, "Hello")
+for n in myli:
+    if n % 2 == 0:
+        print(n, " is an even number.")
+    else:
+        print(n, " is an odd number.")
